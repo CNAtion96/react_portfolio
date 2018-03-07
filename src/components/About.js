c9f1bd4294d70a4e08d2e456ab341587091a9954
Chris Adkins <c.adkins19967@gmail.com>
2018-03-07 17:04:52 -0500

    updated current project

--- src/components/About.js
+++ src/components/About.js
@@ -18,7 +18,7 @@ const About = () => {
                         Currently Reading: The Sword of Truth Series by Terry Goodkind
                     </h3>
                     <h3>
-                        Currently Working on: Intermittent Fasting and Keto tracker built with Vue.js with Node.js backend
+                        Currently Working on: React Native app for Kinobody (personal use)
                     </h3>
                 </div>
                 
